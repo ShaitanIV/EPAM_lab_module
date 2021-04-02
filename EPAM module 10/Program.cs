130a029248c7337963f9a2272588a4fad3a6c05c
ShaitanIV <71263199+ShaitanIV@users.noreply.github.com>
2021-04-02 15:43:07 +0600

    Fixed task location

--- EPAM module 10/Program.cs
+++ EPAM module 10/Program.cs
@@ -1,27 +1,12 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 
-namespace EPAM_module_9
+namespace EPAM_module_8
 {
     class Program
-    {       
+    {
         static void Main(string[] args)
         {
-            List<StudentTest> testList = new List<StudentTest>();
-            SortedList<string, StudentTest> TestBST = new SortedList<string, StudentTest>();
-            string path = @"D:\states.dat";
-            for (int i = 0; i < 10; i++)
-            {
-                testList.Add(new StudentTest("abc" + i, "test", "11.11.11", i * 10));
-            }
-
-            StudentFileManager.Write(testList, path);
-
-            TestBST = StudentFileManager.Read(path);
-
-            StudentDataManager.ShowData(TestBST);
+            Console.WriteLine(ReversePoleNotation.Count("1 2 + 4 * 3 +"));          
         }
     }
 }
